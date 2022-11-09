953b7ed1fe67d40e602c77664ef636baed1048b8
sdatproizv <D.Alavaro@gmail.com>
2022-11-09 17:18:41 +0300

    09/11/2022 windows

--- part1/hidden_method/main.py
+++ part1/hidden_method/main.py
@@ -11,29 +11,42 @@
 #     Внесите соответсвующее изменение в метод add_item чтобы метод работал корректно
 # Стартовый код
 class Item:
-    def __init__(self, title, price, unit, quantity):
+    def __init__(self, title, price, unit, quantity, discount_value=None):
         self.title = title
         self.price = price
         self.unit = unit
         self.quantity = quantity
+        self.discount_value = discount_value
 
     def total_price(self):
+        if self.discount_value:
+            return self._calculate_discount() * self.quantity
         return self.price * self.quantity
 
-class Cheque:
+    def _calculate_discount(self):
+        return self.price * (1 - self.discount_value)
+
+
+class Cheque():
     def __init__(self):
         self.items = []
 
-    def add_item(self, title, price, unit, quantity):
-        item = Item(title=title, price=price, unit=unit, quantity=quantity)
+    def add_item(self, title, price, unit, quantity, discount_value=None):
+        item = Item(
+            title=title,
+            price=price,
+            unit=unit,
+            quantity=quantity,
+            discount_value=discount_value)
         self.items.append(item)
 
     def purchases(self):
-        return "\n".join(
-            [f"{item.title}, {item.quantity} {item.unit} - {item.total_price()}" for item in self.items])
-    
+        item = [f"{item.title}, {item.quantity}{item.unit} - {item.total_price()}" for item in self.items]
+        return "\n".join(item)
+
     def get_sum(self):
         cheque_sum = sum([item.total_price() for item in self.items])
+
         return f"Сумма: {cheque_sum}"
 
 
